b4d867005217f328428760a134c2601ef7afacc2
Valyavkin Maxim <valyavkin99@mail.ru>
2022-12-26 01:16:52 +0300

    first version

---
+++ src/main.js
@@ -0,0 +1,14 @@
+// const path = require('path');
+// const fs = require(path.resolve(__dirname).replace(/\\/g, '/') + '/../node_modules/webpack/lib/util/fs.js');
+
+
+// const distPath = path.resolve(__dirname, 'html');
+
+// fs.readdir(distPath, (err, items) => {
+//     items.filter(item => (/\./.test(item))).forEach(item => {
+//         require(path.resolve(__dirname).replace(/\\/g, '/') + `/html/` + item + '/');
+//     })
+// });
+
+import './html/index.html'
+import './scss/style.scss'
