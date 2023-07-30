c26a04e28015cf3ca946f1cab8fe4ed4af4ffff4
Valyavkin Maxim <valyavkin99@mail.ru>
2023-07-30 14:54:59 +0300

    Added layout change user account

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,10 +1,12 @@
 const Pool = require('pg').Pool;
+require('dotenv').config();
+
 const pool = new Pool({
-    user: 'postgres',
-    password: 'eaglevasasql',
+    user: process.env.dbUser,
+    password: process.env.dbPass,
     host: 'localhost',
     port: 5432,
-    database: 'wow'
+    database: process.env.dbName
 });
 
 module.exports = pool;
